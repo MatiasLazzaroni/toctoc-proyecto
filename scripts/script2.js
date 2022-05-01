8f352da6363f20db4064f4ca6ae83a12283c90c0
Matias Lazzaroni <matias_lazzaroni@hotmail.com>
2022-05-01 19:24:47 -0300

    eliminar scripts2

--- scripts/script2.js
+++
@@ -1,8 +0,0 @@
-/*seccion Armando de proyecto*/
-$(".btnEstilos").on("click",function(){
-    alert("hola")
-    $("#seccionFormulario").show();
-    $(".seccionFormulario").show();
-    $(".seleccionEstilos").hide();
-    $(".tituloEstilos").hide();
-});
