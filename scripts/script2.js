0eb0d770ce3478f7408722e24c5dd93b7389b92f
Matias Lazzaroni <matias_lazzaroni@hotmail.com>
2022-05-01 19:17:27 -0300

    modificando navbar

---
+++ scripts/script2.js
@@ -0,0 +1,8 @@
+/*seccion Armando de proyecto*/
+$(".btnEstilos").on("click",function(){
+    alert("hola")
+    $("#seccionFormulario").show();
+    $(".seccionFormulario").show();
+    $(".seleccionEstilos").hide();
+    $(".tituloEstilos").hide();
+});
